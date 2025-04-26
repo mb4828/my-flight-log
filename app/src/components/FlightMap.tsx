fafd7b59580cfb1104afbaa88747b08fa4a171a3
Matt Brauner <mattbrauner44@gmail.com>
2025-04-25 23:46:26 -0400

    zoom adjust

--- app/src/components/FlightMap.tsx
+++ app/src/components/FlightMap.tsx
@@ -122,7 +122,7 @@ const FlightMap = () => {
         initialViewState={{
           longitude: -74.006,
           latitude: 40.7128,
-          zoom: !IS_MOBILE ? 2 : 0.4,
+          zoom: !IS_MOBILE ? 2 : 0.2,
         }}
         mapStyle="mapbox://styles/mapbox/outdoors-v12"
         mapboxAccessToken={MAPBOX_TOKEN}
